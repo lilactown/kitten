f8b75ebb4ce7727c4202ba1da01be3e920a52ac4
Will Acton <will@amperity.com>
2022-10-18 12:49:19 -0700

    doom-modeline

--- init.el
+++ init.el
@@ -76,6 +76,9 @@
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
+
 ;; Replacements for most completing-read functions
 (use-package consult)
 
@@ -181,3 +184,31 @@
          (display-graphic-p))
      (require 'ns-auto-titlebar nil t)
      (ns-auto-titlebar-mode +1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval define-clojure-indent
+           (:require 0)
+           (:import
+            '(0
+              (0)))
+           (defrecord
+             '(1 nil
+                 (:defn)))
+           (forv 1)
+           (for+ 1)
+           (future-with 1)
+           (start-unless 1)
+           (stop-when 1)
+           (do-at 1)
+           (thrown\? 1)
+           (thrown-with-msg\? 2)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
