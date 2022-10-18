79f8fc388be12bea4babeea02fee96246f4bff64
Will Acton <will@willacton.com>
2022-10-17 23:30:55 -0700

    delete-file-and-buffer

--- keybinds.el
+++ keybinds.el
@@ -32,11 +32,26 @@
  ("B" . consult-project-buffer))
 
 ;; File management
+
+;; based on http://emacsredux.com/blog/2013/04/03/delete-file-and-buffer/
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if filename
+        (if (y-or-n-p (concat "Do you really want to delete file " filename " ?"))
+            (progn
+              (delete-file filename)
+              (message "Deleted file %s." filename)
+              (kill-buffer)))
+      (message "Not a file visiting buffer!"))))
+
 (bind-keys
  :prefix-map kitten/file
  :prefix "C-c f"
  ("f" . find-file)
- ("s" . save-buffer))
+ ("s" . save-buffer)
+ ("d" . delete-file-and-buffer))
 
 ;; Magit
 (bind-keys
