82ecfe4c395254ca2d90438803536dc7bee9339e
Will Acton <will@willacton.com>
2022-11-07 23:47:44 -0800

    include server-start in init

--- user/init.el
+++ user/init.el
@@ -6,9 +6,10 @@
 
 (require 'kitten-emacs)
 (require 'kitten-ui)
-
 (require 'kitten-org)
 (require 'kitten-meow)
+(require 'kitten-project)
+(require 'kitten-completion)
 
 ;; Programming languages
 (use-package markdown-mode)
@@ -16,8 +17,10 @@
 (require 'kitten-git)
 (require 'kitten-lisp)
 (require 'kitten-clojure)
-(require 'kitten-project)
-(require 'kitten-completion)
+
+;; start server to allow emacsclient usage
+(unless (server-running-p)
+  (server-start))
 
 ;; Load keybindings
 (load (expand-file-name "keybinds.el" kitten-user-dir))
