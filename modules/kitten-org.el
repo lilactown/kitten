cff9e04bd22502af8e37c55c40408651482521e4
Will Acton <will@willacton.com>
2022-11-19 08:17:23 -0800

    kitten-org: prioritize agenda view

--- modules/kitten-org.el
+++ modules/kitten-org.el
@@ -71,34 +71,38 @@
 
   ;; agenda
   (setq org-agenda-custom-commands
-      '(("g" "Get Things Done (GTD)"
-         ((agenda ""
-                  (;(org-agenda-skip-function
-                   ;;'(org-agenda-skip-entry-if 'deadline))
-                   (org-deadline-warning-days 0)
-                   (org-agenda-prefix-format " %i %-12:c%?-12t%-6e% s")))
-          (todo "NEXT"
-                ((org-agenda-skip-function
-                  '(org-agenda-skip-entry-if 'deadline 'scheduled))
-                 (org-agenda-prefix-format "  %i %-12:c [%e] ")
-                 (org-agenda-overriding-header "\nTasks\n")))
-          (todo "WAITING"
-               (;;(org-agenda-skip-function
-                ;;  '(org-agenda-skip-entry-if 'deadline))
-                 (org-agenda-prefix-format "  %i %-12:c [%e] ")
-                 (org-agenda-overriding-header "\nWaiting\n")) )
-          (agenda nil
-                  ((org-agenda-entry-types '(:deadline))
-                   (org-agenda-format-date "")
-                   (org-deadline-warning-days 7)
-                   (org-agenda-skip-function
-                    '(org-agenda-skip-entry-if 'notregexp "\\* NEXT"))
-                   (org-agenda-overriding-header "\nDeadlines")))
-          (tags-todo "inbox"
-                     ((org-agenda-prefix-format "  %?-12t% s")
-                      (org-agenda-overriding-header "\nInbox\n")))
-          (tags "CLOSED>=\"<today>\""
-                ((org-agenda-overriding-header "\nCompleted today\n")))))))
+        '(("p" "Prioritize"
+           ((tags-todo "+@urgent+@important")
+            (tags-todo "+@urgent-@important")
+            (tags-todo "+@important-@urgent")))
+          ("g" "Get Things Done (GTD)"
+           ((agenda ""
+                    (;(org-agenda-skip-function
+                     ;;'(org-agenda-skip-entry-if 'deadline))
+                     (org-deadline-warning-days 0)
+                     (org-agenda-prefix-format " %i %-12:c%?-12t%-6e% s")))
+            (todo "NEXT"
+                  ((org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'deadline 'scheduled))
+                   (org-agenda-prefix-format "  %i %-12:c [%e] ")
+                   (org-agenda-overriding-header "\nTasks\n")))
+            (todo "WAITING"
+                  (;;(org-agenda-skip-function
+                   ;;  '(org-agenda-skip-entry-if 'deadline))
+                   (org-agenda-prefix-format "  %i %-12:c [%e] ")
+                   (org-agenda-overriding-header "\nWaiting\n")) )
+            (agenda nil
+                    ((org-agenda-entry-types '(:deadline))
+                     (org-agenda-format-date "")
+                     (org-deadline-warning-days 7)
+                     (org-agenda-skip-function
+                      '(org-agenda-skip-entry-if 'notregexp "\\* NEXT"))
+                     (org-agenda-overriding-header "\nDeadlines")))
+            (tags-todo "inbox"
+                       ((org-agenda-prefix-format "  %?-12t% s")
+                        (org-agenda-overriding-header "\nInbox\n")))
+            (tags "CLOSED>=\"<today>\""
+                  ((org-agenda-overriding-header "\nCompleted today\n")))))))
 
   ;; Copied from https://github.com/minad/org-modern
   (setq
