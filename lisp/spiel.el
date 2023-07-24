c96cd827451d26c629ddeda20c2532654ab50486
Will Acton <will@amperity.com>
2023-07-24 13:27:04 -0700

    output as markdown, store timestamps, define compose mode keymap
    prompt region

--- lisp/spiel.el
+++ lisp/spiel.el
@@ -9,7 +9,7 @@
 (use-package openai
   :straight (:type git :host github :repo "lilactown/openai"))
 
-
+(require 'markdown-mode)
 (require 'openai)
 (require 'openai-chat)
 
@@ -39,7 +39,6 @@
 
 ;; (spiel--write-messages "foo" `[((role . "user") (content . "How are you?"))])
 
-
 (defun spiel--read-messages (topic)
   ""
   (let ((filename (concat spiel-conversation-dir "/" topic)))
@@ -51,37 +50,55 @@
           (read (current-buffer)))
       [])))
 
-
 ;; (spiel--read-messages "foo")
 ;; (spiel--read-messages "bar")
+
+(defun spiel--display-messages (topic messages)
+  ""
+  (let ((buf-name (concat "*Spiel: " topic "*")))
+    (when (get-buffer buf-name)
+      (kill-buffer buf-name))
+    (with-output-to-temp-buffer buf-name
+      (mapc (lambda (msg)
+              (let-alist msg
+                (princ (format "**<%s> %s:** %s\n\n"
+                               (if .time (format-time-string "%D %R" .time) "?")
+                               .role (string-trim .content)))
+                (princ "---\n\n")))
+            (reverse messages)))
+    (with-current-buffer buf-name
+      (markdown-view-mode))))
+
+(defun spiel-show-topic (topic)
+  (interactive "sTopic: ")
+  (spiel--display-messages topic (spiel--read-messages topic)))
 
 (defun spiel--say (topic message)
   ""
   (interactive)
   (let ((messages (vconcat (spiel--read-messages topic)
                            `[((role . "user")
-                              (content . ,message))])))
+                              (content . ,message)
+                              (time . ,(current-time)))])))
     (openai-chat
-     messages
+     ;; remove `time` key
+     (cl-map 'vector
+             (lambda (msg) (assq-delete-all 'time (copy-alist msg)))
+             messages)
      (lambda (data)
        (let ((choices (let-alist data .choices)))
-         (with-output-to-temp-buffer (concat "*Topic: " topic "*")
-           (mapc (lambda (message)
-                   (let-alist message
-                     (princ (format "%s: %s\n\n" .role (string-trim .content)))))
-                 messages)
-           (mapc (lambda (choice)
-                   (let-alist choice
-                     (let-alist .message
-                       (princ (format "%s: %s\n\n" .role (string-trim .content))))))
-                 choices))
-         (spiel--write-messages
-          topic
-          (vconcat messages
-                   (cl-map 'vector
-                        (lambda (choice)
-                          (let-alist choice .message))
-                        choices)))))
+         (let ((messages (vconcat messages
+                                   (cl-map 'vector
+                                           (lambda (choice)
+                                             (let-alist choice
+                                               (push `(time . ,(current-time))
+                                                     .message)
+                                               .message))
+                                           choices))))
+           (spiel--display-messages topic messages)
+           (spiel--write-messages
+            topic
+            messages))))
      :parameters '(("api-version" . "2023-05-15")))))
 
 
@@ -91,22 +108,90 @@
 ;; (spiel--say "testing" "Now pretend you're my son and answer the question.")
 
 
-(defvar-keymap spiel-compose-mode-map
-  :doc "Key map used by `spiel-compose-mode'.")
 
 (define-minor-mode spiel-compose-mode
   "Minor mode used when composing a new message to send to a *spiel* topic."
-  :keymap spiel-compose-mode-map)
+  :init-value t
+  :keymap
+  `((,(kbd "C-c C-c") . spiel-prompt-send-and-exit)
+    (,(kbd "C-c C-k") . spiel-prompt-kill)))
+
+;; (defun spiel--prompt-buffer-topic ()
+;;   "https://stackoverflow.com/a/66592073"
+;;   (nth
+;;    1
+;;    (assoc
+;;     "TITLE"
+;;     (org-element-map (org-element-parse-buffer 'greater-element) '(keyword)
+;;       (lambda (kwd)
+;;         (let ((data (cadr kwd)))
+;;           (list (plist-get data :key)
+;;                 (plist-get data :value))))))))
+
+(defun spiel-prompt-kill ()
+  ""
+  (interactive)
+  (kill-buffer (current-buffer)))
+
+(defun spiel-prompt-send-and-exit ()
+  ""
+  (interactive)
+  (goto-char (point-min))
+  (forward-char 2)
+  (let ((topic (buffer-substring-no-properties (point)
+                                               (line-end-position)))
+        (prompt (progn
+                  (forward-line)
+                  (buffer-substring-no-properties (line-beginning-position) (point-max)))))
+    ;; (message "%s" prompt)
+    (kill-buffer)
+    (spiel--say topic prompt)))
+
+(defun spiel--prompt-buffer-name (topic)
+  ""
+  (concat "*Spiel prompt: " topic "*"))
 
 (defun spiel-prompt (topic)
   ""
-  (interactive "s")
-  (switch-to-buffer (generate-new-buffer-name (concat "*Prompt (" topic ")*")))
-  (org-mode)
-  (spiel-compose-mode))
+  (interactive "sTopic: ")
+  (switch-to-buffer (spiel--prompt-buffer-name topic))
+  (insert "# " topic "\n\n")
+  (with-silent-modifications
+    (put-text-property 1 (- (point) 1) 'read-only t))
+  (spiel-compose-mode 1)
+  (markdown-mode))
+
+
+(defun spiel-prompt-region (topic start end)
+  ""
+  (interactive "sTopic: \nr")
+  (let ((region-text (buffer-substring-no-properties start end))
+        (prompt-buffer (get-buffer (spiel--prompt-buffer-name topic))))
+    (unless prompt-buffer
+      (spiel-prompt topic))
+    (switch-to-buffer (get-buffer (spiel--prompt-buffer-name topic)))
+    (goto-char (point-max))
+    (insert "\n" region-text)))
 
 
 ;; (spiel-prompt "testing")
+
+
+;;
+;; dev
+;;
+
+;; (spiel--say "elisp" "How do I decrement a number in elisp?")
+
+;; (spiel--say "elisp" "In Emacs elisp, how do I reverse a list?")
+
+;; (spiel--say "elisp" "In Emacs elisp, how do I get the buffer contents starting from a certain line to the end?")
+
+
+;; (spiel--say "elisp" "Your last answer was wrong. `point-at-bol` scans forward N lines from the current location the point is in the buffer. How can I get the point at the beginning of line 2?")
+
+;; (spiel--say "elisp" "Your last answer was wrong. `line-beginning-position` scans forward N lines from the current location the point is in the buffer. How can I get the point at the beginning of line 2?")
+
 
 (provide 'spiel)
 
