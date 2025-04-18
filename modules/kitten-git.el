08b18bbcc40779db2fe0ead2ad5f2a4b853871f0
Will Acton <will@willacton.com>
2025-04-18 10:10:02 -0700

    delete forge and code-review

--- modules/kitten-git.el
+++ modules/kitten-git.el
@@ -8,21 +8,6 @@
   :straight (magit :host github :repo "magit/magit" :branch "main")
   :commands magit-status)
 
-(use-package forge
-  :after magit
-  :config
-  (magit-add-section-hook
-   'magit-status-sections-hook
-   'forge-insert-requested-reviews
-   'forge-insert-pullreqs
-   'replace))
-
-(use-package code-review
-  :config
-  (setq code-review-fill-column 80)
-  (setq code-review-auth-login-marker 'code-review)
-  :hook (code-review-mode-hook . #'emojify-mode))
-
 (provide 'kitten-git)
 
 ;;; kitten-git.el ends here.
